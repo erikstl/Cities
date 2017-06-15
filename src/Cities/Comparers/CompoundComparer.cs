86346e9913a30acc0485b8cae17af159a78c4f36
Erik Bates <erik.s.bates@gmail.com>
2017-06-15 18:56:16 -0500

    Finished base. No bonus.

---
+++ src/Cities/Comparers/CompoundComparer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cities.Comparers
+{
+    class CompoundComparer : IComparer<City>
+    {
+        
+    }
+}
---
+++ src/Cities/Comparers/PopulationComparer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cities.Comparers
+{
+    class PopulationComparer : IComparer<City>
+    {
+        public int Compare(City x, City y)
+        {
+            // Since we want to sort strings, we can use the
+            // comparer for the built-in string class
+            return (x.Population.CompareTo(y.Population));
+        }
+    }
+}
